80f9308aabd4c263a3817a4c357e773412d9268a
Sonja Parsell <hello@sparsell.com>
2020-04-02 12:50:33 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -6,9 +6,15 @@
 
 def happy_birthday(birthday_kids)
   birthday_kids.each do |kids_name, age|
-    #if age <= 12
     puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
-    #else
-    #end
   end
 end
+
+def age_appropriate_birthday(birthday_kids)
+    birthday_kids.each do |kids_name, age|
+      if age <= 12
+      puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
+      else
+      end
+    end
+  end
